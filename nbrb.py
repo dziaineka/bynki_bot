22a6998152a023f0294462f890dfc931791e5efa
Andrei Dziaineka <dziaineka@gmail.com>
2023-08-05 13:58:07 +0300

    add more currencies

--- nbrb.py
+++ nbrb.py
@@ -39,7 +39,9 @@ async def _get_rate(currency_type):
         async with http_session.get(url) as response:
             if response.status != 200:
                 raise Exception(
-                    f"get rates response status is {response.status}"
+                    f"get rates response status is {response.status}. "
+                    f"Response: {await response.json()} "
+                    f"URL: {url}"
                 )
 
             resp_json = await response.json(content_type=None)
